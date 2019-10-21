3dc7abcf77a56dcecba48a488ea59f87d8d6607b
Jishnu P <jishnu.jayakumar182@gmail.com>
2019-10-21 21:37:56 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='pyIUDX',
-    version='0.0.13',
+    version='0.0.1',
     packages=[''],
     install_requires=['requests', 'urllib3'],
     url='https://github.com/rbccps-iisc/pyIUDX',
     license='GNU General Public License v3.0',
-    author='Mukunth A, Jishnu P',
-    author_email='mukuntharun@gmail.com, jishnu.jayakumar182@gmail.com',
+    author='Mukunth A, Jishnu P, Arun Babu, Rakshit Ramesh',
+    author_email='mukuntharun@gmail.com, jishnu.jayakumar182@gmail.com, barun@iisc.ac.in, rakshitr@iisc.ac.in',
     description='Python SDK for IUDX.'
     )
